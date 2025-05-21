a83643df8c872dfe84f4023f0a8df62e27d7b71d
kwiha <kwiha@mail.ru>
2025-05-21 15:45:20 -0700

    Enhance main.py by adding MCP client session setup and integrating ChatOpenAI. Introduced StdioServerParameters for server command execution.

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import asyncio, os
+
 from dotenv import load_dotenv  
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
 
 load_dotenv()
-# print(os.getenv("OPENAI_API_KEY"))
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["C:\\Users\\test\\Documents\\mcp-learning\\servers\\math_server.py"],
+)
 
 async def main():
     print("Hello from mcp-learning!")
